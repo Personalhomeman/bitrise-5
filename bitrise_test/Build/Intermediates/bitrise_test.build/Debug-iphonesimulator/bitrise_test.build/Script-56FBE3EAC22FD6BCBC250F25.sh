e4691425b36a98c63770661d01b6a8f8ea092c2d
t.kaneko <nontan20xx@gmail.com>
2018-06-12 14:03:27 +0900

    Initial commit

---
+++ bitrise_test/Build/Intermediates/bitrise_test.build/Debug-iphonesimulator/bitrise_test.build/Script-56FBE3EAC22FD6BCBC250F25.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+"${SRCROOT}/Pods/Target Support Files/Pods-bitrise_test/Pods-bitrise_test-frameworks.sh"
+
---
+++ bitrise_test/bitrise_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bitrise_test
+//
+//  Created by Takahiro Kaneko on 2018/06/12.
+//  Copyright © 2018年 TK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
